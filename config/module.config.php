588c4176d8791124a95c3dadb851089e1ea01074
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-05-30 11:57:09 +0200

    add first theme

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,19 @@
 <?php
 return array(
-
+    'templates' => array(
+        'themes' => array(
+            'formation' => array(
+                'label' => 'Formation',
+                'basePath' => realpath(__DIR__ . '/../themes/formation'),
+                'noBootstrap' => true,
+                'css' => array(
+                    '/css/bootstrap.min.css',
+                ),
+                'js' => array(
+                ),
+                'img' => array(
+                )
+            ),
+        )
+    ),
 );
